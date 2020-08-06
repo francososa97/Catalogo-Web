76f8828a5314aaa69f45f1bf230210249a2e0536
francososa97 <franco.gonzalez.sosa@gmail.com>
2020-08-06 18:59:03 +0200

    Desarrollo de sitio

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Component/Navbar';
+import Footer from './Component/Footer';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/Navbar.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav,MDBHamburgerToggler  ,MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBContainer, MDBMask, MDBView } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false,
+      collapse1: false,
+      collapseID: ''
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+  toggleCollapse = collapseID => () => {
+    this.setState(prevState => ({ collapseID: (prevState.collapseID !== collapseID ? collapseID : '') }));
+  }
+  
+  toggleSingleCollapse = collapseId => {
+    this.setState({
+      ...this.state,
+      [collapseId]: !this.state[collapseId]
+    });
+  }
+
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <Router>
+            <MDBNavbar color="blue lighten-1">
+            <MDBNavbarBrand>
+              Franco Sosa
+            </MDBNavbarBrand>
+            <MDBHamburgerToggler color="white" id="hamburger1" onClick={()=> this.toggleSingleCollapse('collapse1')} />
+              <MDBCollapse isOpen={this.state.collapse1} navbar>
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                    <MDBNavLink to="#!">Home</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Catalogo</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Contacto</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+        </MDBNavbar>
+          </Router>
+
+          <MDBView src="https://mdbootstrap.com/img/Photos/Others/img%20(50).jpg">
+            <MDBMask overlay="black-light" className="flex-center flex-column text-white text-center">
+              <h2>This Navbar is fixed</h2>
+              <h5>It will always stay visible on the top, even when you scroll down</h5>
+              <br />
+              <p>Full page intro with background image will be always displayed in full screen mode, regardless of device </p>
+            </MDBMask>
+          </MDBView>
+        </header>
+
+        <main>
+          <MDBContainer className="text-center my-5">
+            <p align="justify">Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </MDBContainer>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
