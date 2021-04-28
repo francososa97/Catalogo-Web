3310f563114b4a2b2f90a57dab3b6f4d6b0b110f
francososa97 <franco.gonzalez.sosa@gmail.com>
2021-04-28 12:17:00 +0200

    fix: prod

--- src/context/CatalogueContext.js
+++ src/context/CatalogueContext.js
@@ -1,6 +1,16 @@
 import React, { createContext, useState, useEffect } from 'react';
 import { db } from "../FireBase/FirebaseConfig";
-
+import store from '../Images/store.PNG';
+import veterinarian from '../Images/veterinario.PNG';
+import br from '../Images/br.PNG';
+import song from '../Images/canciones.PNG';
+import weather from '../Images/clima.PNG';
+import budget from '../Images/presupuesto.PNG';
+import coin from '../Images/coin.PNG';
+import CamelCase from '../Images/CamelCase.PNG';
+import ListadoCustomer from '../Images/ListadoCustomer.PNG';
+import Reservas from '../Images/Reservas.PNG';
+import reproductor from '../Images/reproductor.PNG';
 
 export const CatalogueContext = createContext();
 
