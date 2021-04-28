5f8ef3d4b0ab1df4b4b19917aec82fa3de8438e5
francososa97 <franco.gonzalez.sosa@gmail.com>
2021-04-28 11:26:37 +0200

    feat: img

--- src/Component/BlogPage.js
+++ src/Component/BlogPage.js
@@ -3,6 +3,17 @@ import React, { useContext } from 'react';
 import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBMask, MDBIcon, MDBView, MDBBtn} from "mdbreact";
 import { CatalogueContext } from '../context/CatalogueContext';
 import git from '../Images/iconfinder_github_3.png';
+import store from '../Images/store.PNG';
+import veterinarian from '../Images/veterinario.PNG';
+import br from '../Images/br.PNG';
+import song from '../Images/canciones.PNG';
+import weather from '../Images/clima.PNG';
+import budget from '../Images/presupuesto.PNG';
+import coin from '../Images/coin.PNG';
+import CamelCase from '../Images/CamelCase.PNG';
+import ListadoCustomer from '../Images/ListadoCustomer.PNG';
+import Reservas from '../Images/Reservas.PNG';
+import reproductor from '../Images/reproductor.PNG';
 
 
 const BlogPage = () => {
